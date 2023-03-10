8413700dffa272c2c4ce6e79c00e1744f408d31b
roms <rusirusamaraweera749@gmail.com>
2023-03-10 09:59:58 +0530

    add delete feature

--- index.js
+++ index.js
@@ -205,6 +205,12 @@ app.put('/products/:id', async(req,res) => {
 
 })
 
+app.delete('/products/:id' , async (req,res) =>{
+  const {id} = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id)
+  res.redirect('/products');
+})
+
 
 app.listen(3000, () => {
   console.log("App is Listening on port 3000");
